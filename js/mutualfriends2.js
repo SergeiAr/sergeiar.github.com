f761db3158271b12832f37948b74fd22a2895b37
SergeiAr <fooktool@gmail.com>
2015-02-16 15:01:05 +0400

    5 comm

--- js/mutualfriends2.js
+++ js/mutualfriends2.js
@@ -1,6 +1,10 @@
 VK.api("users.get", {fields:"photo_50"}, function(data) {
     // Действия с полученными данными
-	var api_result = JSON.parse($_GET('api_result'));
+	  if (data.response) {
+  // data.response is object
+  
+	var api_result = JSON.parse($_GET('data'));
 	alert(api_result);
 	$('.photo_result_api').html('<img src="' + api_result.response[0].photo_50 + '" alt="" class="img-responsive">');
+	  }
 }); 
