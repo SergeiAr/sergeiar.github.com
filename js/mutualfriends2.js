1eb916759c2de6135c0572e45777d6e06f146464
SergeiAr <fooktool@gmail.com>
2015-02-17 18:54:36 +0400

    38
    бар прогресс

--- js/mutualfriends2.js
+++ js/mutualfriends2.js
@@ -99,7 +99,7 @@ function AddUser(user_id) {
 									+ '<div class="clearfix"></div>'
 								+ '</li>'
 									+ '<div class="progress progress-striped active">'
-									  + '<div id="progress" class="progress-bar" role="progressbar" aria-valuenow="'+ 2*100 +'" aria-valuemin="0" aria-valuemax="100" style="width: '+ (2/1000)*100 +'%">'
+									  + '<div id="progress" class="progress-bar" role="progressbar" aria-valuenow="'+ i +'" aria-valuemin="0" aria-valuemax="100" style="width: '+ (2/1000)*100 +'%">'
 										+ '<span class="sr-only">'+ 2*100 +'% Complete</span>'
 									 + '</div>'
 									+ '</div>'
